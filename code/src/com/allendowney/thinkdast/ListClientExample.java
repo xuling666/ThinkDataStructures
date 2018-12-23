ec9fc5d7cbdc1ff112fadd95beba4d1127af8cdc
xuling666 <44254893+xuling666@users.noreply.github.com>
2018-12-23 15:32:56 +0800

    Update ListClientExample.java

--- code/src/com/allendowney/thinkdast/ListClientExample.java
+++ code/src/com/allendowney/thinkdast/ListClientExample.java
@@ -4,12 +4,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ListClientExample {
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings("rawtypes") //SuppressWarnings压制警告，即忽略警告,rawtypes是说传参时也要传递带泛型的参数
 	private List list;
 
 	@SuppressWarnings("rawtypes")
 	public ListClientExample() {
-		list = new LinkedList();
+		list = new LinkedList(); //面向接口的编程
 	}
 
 	@SuppressWarnings("rawtypes")
